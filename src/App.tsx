e2af7f65615b1d94d70f35cb04a5cfc1a9be8c83
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-03 16:24:47 +0200

    feat: Added state for user authentication.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import LoginPage from './components/Auth/AuthPage';
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false);
+
+  const content = !isAuthenticated ? <LoginPage /> : <p>Zalogowano.</p>
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {content}
     </div>
   );
 }
