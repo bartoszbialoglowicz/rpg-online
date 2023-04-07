ca1d36070c28e41025e7ba069f84303ff6ebe4a0
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-07 15:07:08 +0200

    test: Testing for use http

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,9 +1,15 @@
+import { useHttp } from "../../hooks/use-http";
 import SubmitInput from "../UI/SubmitInput";
 import InputLabel from "./InputLabel";
 
 const LoginForm = () => {
+    const loginHandler = () => {
+        sendRequest();
+    }
+    const sendRequest = useHttp('api/token/', 'POST');
+
     return <div className="login-form">
-        <form>
+        <form onSubmit={loginHandler}>
             <InputLabel type="text" label="E-mail address" name="email" />
             <InputLabel type="password" label="Password" name="password" />
             <SubmitInput text="SIGN IN" />
