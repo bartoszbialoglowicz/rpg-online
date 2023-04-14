d02f8125fea1f6eae54bf0e8c837b4d8cd50652d
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-14 21:24:38 +0200

    feat: Added email, password and nickname regex

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,3 +1,6 @@
 export class AppSettings {
     public static readonly SERVER_IP = '192.168.0.143:8000';
+    public static readonly PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    public static readonly EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    public static readonly NICKNAME_REGEX = /^(?!.*\d)[\s\S]{6,16}$/;
 }
