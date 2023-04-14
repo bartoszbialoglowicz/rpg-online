c369d81eb02708e2eae1e7d5231ebc443633b612
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-14 21:21:53 +0200

    feat: Added protytype login handler

--- src/components/Auth/LoginForm.tsx
+++ src/components/Auth/LoginForm.tsx
@@ -1,17 +1,34 @@
+import { ChangeEvent, FormEvent, useState } from "react";
 import { useHttp } from "../../hooks/use-http";
 import SubmitInput from "../UI/SubmitInput";
 import InputLabel from "./InputLabel";
 
 const LoginForm = () => {
-    const loginHandler = () => {
+    const [emailValue, setEmailValue] = useState('');
+    const [passwordValue, setPasswordValue] = useState('');
+
+    const sendRequest = useHttp(
+        'api/token/', 
+        'POST', 
+        {'email': emailValue, 'password': passwordValue}
+    );
+
+    const loginHandler = (event: FormEvent) => {
+        event.preventDefault();
         sendRequest();
     }
-    const sendRequest = useHttp('api/token/', 'POST');
+
+    const getEmailInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+        setEmailValue(event.target.value);
+    };
+    const getPasswordInputValue = (event: ChangeEvent<HTMLInputElement>) => {
+        setPasswordValue(event.target.value);
+    };
 
     return <div className="login-form">
         <form onSubmit={loginHandler}>
-            <InputLabel type="text" label="E-mail address" name="email" />
-            <InputLabel type="password" label="Password" name="password" />
+            <InputLabel type="text" label="E-mail address" name="email" value={emailValue} onChange={getEmailInputValue}/>
+            <InputLabel type="password" label="Password" name="password" value={passwordValue} onChange={getPasswordInputValue}/>
             <SubmitInput text="SIGN IN" />
         </form>
     </div>
