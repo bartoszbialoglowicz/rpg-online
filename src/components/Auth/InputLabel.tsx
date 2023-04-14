01c1ed690c1b8e39378de655f42df3424e1d02bc
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-14 21:20:35 +0200

    feat: Added onChange prop into Input container

--- src/components/Auth/InputLabel.tsx
+++ src/components/Auth/InputLabel.tsx
@@ -1,9 +1,10 @@
-type inputType = 'email' | 'text' | 'password';
+import { ChangeEvent } from "react";
+import { inputType } from "../../utils/types";
 
-const InputLabel: React.FC<{name: string, type: inputType, label: string}> = (props) => {
+const InputLabel: React.FC<{name: string, type: inputType, label: string, value?: string, onChange?: (event: ChangeEvent<HTMLInputElement>) => void}> = (props) => {
     return <div className="form-input-label">
         <label htmlFor={props.name}>{[props.label]}</label>
-        <input type={props.type} name={props.name}/>
+        <input type={props.type} name={props.name} value={props.value} onChange={props.onChange}/>
     </div>
 };
 
