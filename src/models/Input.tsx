384274975174e3bbdb497eb77bb152e086eee2ae
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-14 21:23:30 +0200

    feat: Added Input model

---
+++ src/models/Input.tsx
@@ -0,0 +1,44 @@
+import { ChangeEvent } from "react";
+import { inputType } from "../utils/types";
+import InputLabel from "../components/Auth/InputLabel";
+import { AppSettings } from "../utils/settings";
+
+class Input {
+    label: string;
+    type: inputType;
+    name: string;
+    value: string;
+    isValid: boolean;
+    defaultValidator: () => boolean;
+    onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
+
+    private setValidator = () => {
+        if (this.type === 'email') {
+            return () => AppSettings.EMAIL_REGEX.test(this.value);
+        }
+        else if (this.type === 'password') {
+            return () => AppSettings.PASSWORD_REGEX.test(this.value);
+        }
+        return () => AppSettings.NICKNAME_REGEX.test(this.value);
+    }
+
+    constructor(label: string, type: inputType, name: string, value: string, isValid: boolean, onChange: (event: ChangeEvent<HTMLInputElement>) => void) {
+        this.label = label;
+        this.type = type;
+        this.name = name;
+        this.isValid = isValid;
+        this.value = value ? value : '';
+        this.onChange = onChange;
+        this.defaultValidator = this.setValidator();
+    }
+
+    public getJSXElement = () => {
+        return <InputLabel label={this.label} type={this.type} name={this.name} value={this.value} onChange={this.onChange} />
+    }
+    
+    public validate = (fn?: ()=> boolean) => {
+        this.isValid = fn ? fn() : this.defaultValidator();
+    }
+};
+
+export default Input;
