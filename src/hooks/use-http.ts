410f9ffe884891a4348676452c2371d817f51e07
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-14 21:23:08 +0200

    feat: Added headers into http requests

--- src/hooks/use-http.ts
+++ src/hooks/use-http.ts
@@ -1,4 +1,3 @@
-import { useEffect, useState } from "react";
 import { AppSettings } from "../utils/settings";
 import { HttpMethod } from "../utils/types";
 
@@ -6,6 +5,11 @@ export const useHttp = (path: string, method: HttpMethod, body?: object) => {
     const sendRequest = async () => {
         const response = await fetch(`http://${AppSettings.SERVER_IP}/${path}`, {
             method: method,
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+                'Authorization': ''
+            },
             body: body ? JSON.stringify(body) : null
         });
         const data = await response.json();
