f9c63f6c284ba95091816635ce386d2fd91e1a2e
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:06:16 +0200

    feat: Added property for managing css class

--- src/components/Auth/InputLabel.tsx
+++ src/components/Auth/InputLabel.tsx
@@ -1,10 +1,20 @@
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
 import { inputType } from "../../utils/types";
 
-const InputLabel: React.FC<{name: string, type: inputType, label: string, value?: string, onChange?: (event: ChangeEvent<HTMLInputElement>) => void}> = (props) => {
-    return <div className="form-input-label">
+import './InputLabel.css';
+
+const InputLabel: React.FC<{name: string, type: inputType, label: string, value?: string, onChange: (event: ChangeEvent<HTMLInputElement>) => void, isValid: boolean}> = (props) => {
+    const [isTouched, setIsTouched] = useState(false);
+    const cssClass = !isTouched ? "form-input-label" : (!props.isValid ? "form-input-label invalid" : "form-input-label");
+
+    const onChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        setIsTouched(true);
+        props.onChange(event);
+    }
+
+    return <div className={cssClass}>
         <label htmlFor={props.name}>{[props.label]}</label>
-        <input type={props.type} name={props.name} value={props.value} onChange={props.onChange}/>
+        <input type={props.type} name={props.name} value={props.value} onChange={onChangeHandler}/>
     </div>
 };
 
