473f1ada0d0d13ae8fbe252af854781e17fb430a
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:18:27 +0200

    feat: Added AuthObject type

--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -1,3 +1,11 @@
+import User from "../models/User";
+
 export type feedbackResult = 'success' | 'error';
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH';
 export type inputType = 'email' | 'text' | 'password';
+export type authContextObject = {
+    isAuthenticated: boolean,
+    user: User,
+    login: (user: User) => void,
+    logout: () => void
+};
