0629f634693b0030f84bbc7e983ec5810e520880
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:12:26 +0200

    feat: Added content container with styles

---
+++ src/components/Main/MainContentContainer.tsx
@@ -0,0 +1,10 @@
+import './MainContentContainer.css';
+import MainNewsContainer from './MainNewsContainer';
+
+const MainContentContainer = () => {
+    return <div className="main-content-container">
+        <MainNewsContainer />
+    </div>
+};
+
+export default MainContentContainer;
