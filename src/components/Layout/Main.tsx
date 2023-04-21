ca3dd8fb1f02505ac305fd61d1db5f24f09ad92d
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:11:44 +0200

    feat: Added main component with styles

---
+++ src/components/Layout/Main.tsx
@@ -0,0 +1,10 @@
+import MainContentContainer from '../Main/MainContentContainer';
+import './Main.css';
+
+const Main = () => {
+    return <div className="main">
+        <MainContentContainer />
+    </div>
+};
+
+export default Main;
