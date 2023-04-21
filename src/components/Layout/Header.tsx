148a14005bd8cd45ca0f613f35401c5b78054705
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:11:08 +0200

    Feat: Added header component with styles

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,15 @@
+import { useContext } from "react";
+import { UserContext } from "../../store/user-context";
+import './Header.css';
+
+const Header = () => {
+    const userCtx = useContext(UserContext);
+    
+    return <div className="header">
+        <header>
+            <p>Hello {userCtx.user.nickname}</p>
+        </header>
+    </div>
+};
+
+export default Header;
