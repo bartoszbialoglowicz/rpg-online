0ce62b30270032b5822a973817b06d33da214d10
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:10:49 +0200

    feat: Input rendered based on model method.

--- src/components/Auth/RegisterForm.tsx
+++ src/components/Auth/RegisterForm.tsx
@@ -1,7 +1,5 @@
-import { ChangeEvent, FormEvent, useState } from "react";
+import { ChangeEvent, useState } from "react";
 import { feedbackResult } from "../../utils/types";
-import SubmitInput from "../UI/SubmitInput";
-import InputLabel from "./InputLabel";
 import { useHttp } from "../../hooks/use-http";
 import Input from "../../models/Input";
 import Form from "../../models/Form";
@@ -25,17 +23,12 @@ const RegisterForm: React.FC<{setFeedbackHandler: (text:string, type:feedbackRes
     const setPassword2InputValue = (event: ChangeEvent<HTMLInputElement>) => {
         setPassword2Value(event.target.value);
     }
-
-    //const emailInput = new Input("E-mail address", "email", "email", emailValue, false, setEmailInputValue);
-    //const nicknameInput = new Input("Nickname", "text", "nickname", nicknameValue, false, setNicknameInputValue);
-    //const password1Input = new Input("Password", "password", "password1", password1Value, false, setPassword1InputValue); 
-    //const password2Input = new Input("Repeat password", "password", "password2", password2Value, false, setPassword2InputValue)
-    
     
     const sendRequest = useHttp(
         'api/createuser/', 
         'POST',
         {'email': 'dupa'})
+
     // Submit form event, provide feedback into controller and render login form
     const onSubmitHandler = () => {
         props.setFeedbackHandler(dummy_text, 'success');
@@ -47,16 +40,8 @@ const RegisterForm: React.FC<{setFeedbackHandler: (text:string, type:feedbackRes
         new Input("Nickname", "text", "nickname", nicknameValue, false, setNicknameInputValue),
         new Input("Password", "password", "password1", password1Value, false, setPassword1InputValue),
         new Input("Repeat password", "password", "password2", password2Value, false, setPassword2InputValue)
-    ], "SIGN UP!",onSubmitHandler);
-    /*
-    <form onSubmit={onSubmitHandler}>
-            {emailInput.getJSXElement()}
-            <InputLabel label="Nickname" type="text" name="nickname" value={nicknameValue} onChange={setNicknameInputValue}/>
-            <InputLabel label="Password" type="password" name="password1" value={password1Value} onChange={setPassword1InputValue}/>
-            <InputLabel label="Repeat password" type="password" name="password2" value={password2Value} onChange={setPassword2InputValue}/>
-            <SubmitInput text="SIGN UP!"/>
-        </form>
-    */
+    ], "SIGN UP!",onSubmitHandler, false);
+
     return <div className="register-form">
         {form.render()}
     </div>
