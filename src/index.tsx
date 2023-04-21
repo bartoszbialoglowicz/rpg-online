ab1e0c146435d9d0116782312f691f1d28dd06ad
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-21 21:04:27 +0200

    feat: Created authentication context

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import UserContextProvider from './store/user-context';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <UserContextProvider>
+      <App />
+    </UserContextProvider>
   </React.StrictMode>
 );
 
