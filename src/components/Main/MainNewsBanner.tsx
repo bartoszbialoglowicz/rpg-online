4a6165fcb0c9b109d92494b73b0ee3a2d38c100a
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-22 20:43:08 +0200

    feat: Added text on news banner

--- src/components/Main/MainNewsBanner.tsx
+++ src/components/Main/MainNewsBanner.tsx
@@ -1,8 +1,12 @@
 import './MainNewsBanner.css';
 
-const MainNewsBanner: React.FC<{path: string}> = (props) => {
+const MainNewsBanner: React.FC<{path: string, text: string, title: string}> = (props) => {
     return <div className="main-news-banner">
         <img src={props.path} alt=""></img>
+        <div className='main-news-banner-text'>
+            <h1>{props.title}</h1>
+            <p>{props.text}</p>
+        </div>
     </div>
 };
 
