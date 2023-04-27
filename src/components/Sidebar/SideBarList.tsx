823af6f9af531ac12ee9aea9d923d622250c47a7
Bartoszko <bartosz.bialoglowicz@gmail.com>
2023-04-27 11:54:11 +0200

    feat: Added character item into list

--- src/components/Sidebar/SideBarList.tsx
+++ src/components/Sidebar/SideBarList.tsx
@@ -1,12 +1,15 @@
 import SideBarListItem from "./SideBarListIItem";
 
 import news from '../../assets/svgs/news-svgrepo-com.svg';
+import character from '../../assets/svgs/user-svgrepo-com.svg';
 import './SideBarList.css';
+import { mainContentName } from "../../utils/types";
 
-const SideBarList = () => {
+const SideBarList: React.FC<{setCurrentMainContent: (name: mainContentName) => void}> = (props) => {
 
     const tmpList = [
-        <SideBarListItem iconPath={news} name="NEWS" />
+        <SideBarListItem iconPath={news} name="NEWS" setCurrentMainContent={props.setCurrentMainContent}/>,
+        <SideBarListItem iconPath={character} name="CHARACTER" setCurrentMainContent={props.setCurrentMainContent}/>
     ]
 
     return <div className="sidebar-list">
