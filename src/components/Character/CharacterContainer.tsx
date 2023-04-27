be9fb0faeab1d6a9828eb2274c00959a685b65dd
Bartoszko <bartosz.bialoglowicz@gmail.com>
2023-04-27 11:55:03 +0200

    feat: Created character container component

---
+++ src/components/Character/CharacterContainer.tsx
@@ -0,0 +1,9 @@
+import ResourceContainer from "./ResourceContainer";
+
+const CharacterContainer = () => {
+    return <div>
+        <ResourceContainer />
+    </div>
+};
+
+export default CharacterContainer;
