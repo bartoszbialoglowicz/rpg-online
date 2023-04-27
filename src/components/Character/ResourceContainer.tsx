6bd846e8e4ef759567a96ba84ac88d72bcf94189
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-04-28 00:24:16 +0200

    feat: Added exp and lvl resources components

--- src/components/Character/ResourceContainer.tsx
+++ src/components/Character/ResourceContainer.tsx
@@ -1,24 +1,33 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useHttp } from "../../hooks/use-http";
 import ResourceItem from "./ResourceItem";
 import { UserContext } from "../../store/user-context";
+import { resurcesResponse } from "../../utils/types";
 
 const ResourceContainer = () => {
 
+    const [gold, setGold] = useState(0);
+    const [lvl, setLvl] = useState(0);
+    const [exp, setExp] = useState(0);
     const userCtx = useContext(UserContext);
-
-    const sendRequest = useHttp('api/resources', 'GET', undefined, userCtx.user.authToken);
+    const sendRequest = useHttp<resurcesResponse>('api/resources', 'GET', undefined, userCtx.user.authToken);
 
     useEffect(() => {
         const fetchData = async () => {
             const {data, code} = await sendRequest();
-            console.log([data, code]);
+            if (code === 200) {
+                setGold(data[0].gold)
+                setExp(data[0].exp)
+                setLvl(data[0].lvl)
+            }
         }
         fetchData();
     }, [])
 
     return <div className="character-resource-container">
-        <ResourceItem name="GOLD" value={100}/>
+        <ResourceItem name="GOLD" value={gold}/>
+        <ResourceItem name="LVL" value={lvl}/>
+        <ResourceItem name="EXP" value={exp}/>
     </div>
 };
 
