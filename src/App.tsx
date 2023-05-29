cb090a69faf1b2b4cd0a5b15ecb7b1cc837e5538
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-05-29 20:51:34 +0200

    refactor: reversed logic for content render

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
-import { useContext, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import './App.css';
 import LoginPage from './components/Auth/AuthPage';
 import { UserContext } from './store/user-context';
 import Layout from './components/Layout/Layout';
+import AuthPage from './components/Auth/AuthPage';
 
 function App() {
   const userCtx = useContext(UserContext);
 
-  const content = !userCtx.isAuthenticated ? <LoginPage /> : <Layout />
+  const content = userCtx.isAuthenticated ? <Layout /> :<AuthPage />;
   return (
     <div className="App">
       {content}
