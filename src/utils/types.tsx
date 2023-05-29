de5679e6d2abbb0152ad4ac558339851b3325672
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-05-29 20:50:50 +0200

    feat: Added Item and Equipment types

--- src/utils/types.tsx
+++ src/utils/types.tsx
@@ -3,13 +3,21 @@ import User from "../models/User";
 export type feedbackResult = 'success' | 'error';
 export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH';
 export type inputType = 'email' | 'text' | 'password';
-export type mainContentName = 'CHARACTER' | 'NEWS';
+export type mainContentName = 'CHARACTER' | 'NEWS' | 'EQUIPMENT';
 export type authContextObject = {
     isAuthenticated: boolean,
-    user: User,
+    user: User | null,
     login: (user: User) => void,
     logout: () => void
 };
+export type authState = {
+    user: User | null;
+    isAuthenticated: boolean;
+}
+export type Action = {
+    type: 'LOGIN_USER' | 'LOGOUT_USER' | 'SET_IS_AUTHENTICATED';
+    payload?: any;
+}
 export type responseObject<objType> = {
     code: number,
     data: objType
@@ -29,4 +37,30 @@ export type resurcesResponse = [{
     gold: number,
     lvl: number,
     exp: number
-}]
+}];
+export type ItemType = 'weapon' | 'helmet' | 'armor' | 'gloves' |'trousers' | 'boots';
+export type Item = {
+    name: string;
+    itemType: ItemType;
+    armor: number;
+    magicResist: number;
+    health: number;
+    damage: number;
+};
+export type Equipment = {
+    weapon?: Item;
+    helmet?: Item;
+    armor?: Item;
+    gloves?: Item;
+    trousers?: Item;
+    boots?: Item;
+}
+export type EquipmentResponseObject = {
+    slot: ItemType;
+    item: Item;
+};
+export type UserItemsResponseObject = {
+    id: number;
+    user: number;
+    item: Item[];
+}
