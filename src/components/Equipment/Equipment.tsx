b1ecec58f10c72428757529e6c9882dde6847195
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-05-29 20:57:19 +0200

    feat: Added equipment component

---
+++ src/components/Equipment/Equipment.tsx
@@ -0,0 +1,11 @@
+import EquipmentItemsContainer from "./EquipmentItemsContainer";
+import UserItemContainer from "./UserItemContainer";
+
+const Equipment = () => {
+    return <div className="equipment">
+        <EquipmentItemsContainer />
+        <UserItemContainer />
+    </div>
+};
+
+export default Equipment;
