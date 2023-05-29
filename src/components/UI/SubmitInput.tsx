9de273e07e6c0e2f74d1849877b359453821fdc7
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-05-29 20:55:12 +0200

    feat: Added styles for submit input

--- src/components/UI/SubmitInput.tsx
+++ src/components/UI/SubmitInput.tsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import './SubmitInput.css';
+
 const SubmitInput: React.FC<{text: string}> = (props) => {
     return <div className="submit-input-container">
         <input type="submit" value={props.text}/>
