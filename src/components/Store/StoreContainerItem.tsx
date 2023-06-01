8a275446e67553629fefad7a600ee163ca8d01ec
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-06-02 00:39:20 +0200

    refactor: Renamed css class

--- src/components/Store/StoreContainerItem.tsx
+++ src/components/Store/StoreContainerItem.tsx
@@ -1,7 +1,7 @@
 import { Store } from "../../utils/types"
 
 const StoreContainerItem: React.FC<Store> = (props) => {
-    return <div className="store-container-store">
+    return <div className="store-container-item">
         <h3>{props.name}</h3>
         <h5>{props.type}</h5>
     </div>
