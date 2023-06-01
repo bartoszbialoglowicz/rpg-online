42e9f8eab7596a87b11bb42ea9863fce472a3dd0
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-06-02 00:38:53 +0200

    feat: Added Collectable Item component

---
+++ src/components/Equipment/EquipmentCollectableItem.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import { CollectableItem } from "../../utils/types";
+import ItemStats from "../UI/ItemStats";
+
+const EquipmentCollectableItem: React.FC<CollectableItem> = (props) => {
+    const [isHidden, setIsHidden] = useState(true);
+
+    const onMouseHandler = (event: React.MouseEvent<HTMLDivElement>) => {
+        setTimeout(() => setIsHidden((prevState: boolean) => !prevState), 200);
+    }
+    
+    return <div className="equipment-container-item" onMouseEnter={onMouseHandler} onMouseLeave={onMouseHandler}>
+        <p>{props.name}</p>
+        <p>Item</p>
+        <ItemStats
+            goldValue={props.goldValue}
+            hidden={isHidden}/>
+    </div>
+}
