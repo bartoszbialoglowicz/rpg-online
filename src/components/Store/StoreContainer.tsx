96b0f35615c2717f1f14828793b303a320524818
Bartoszko <bartosz.bialoglowicz@gmail.com>
2023-06-01 16:46:54 +0200

    fix: Logout while user is not authorized

--- src/components/Store/StoreContainer.tsx
+++ src/components/Store/StoreContainer.tsx
@@ -4,13 +4,14 @@ import { useHttp } from "../../hooks/use-http";
 import { UserContext } from "../../store/user-context";
 import { Store } from "../../utils/types";
 import StoreContainerItem from "./StoreContainerItem";
+import AvailableStoreList from "./AvailableStoreList";
 
 const StoreContainer = () => {
 
     const [stores, setStores] = useState<Store[]>([]);
     const gameCtx = useContext(GameContext);
-    const token = useContext(UserContext).user!.authToken;
-    const sendRequest = useHttp<Store[]>('api/store', 'GET', undefined, token);
+    const userCtx = useContext(UserContext);
+    const sendRequest = useHttp<Store[]>('api/store', 'GET', undefined, userCtx.user!.authToken);
 
 
     useEffect(() => {
@@ -19,6 +20,8 @@ const StoreContainer = () => {
 
             if (code === 200) {
                 setStores(data);
+            } else if (code === 401) {
+                userCtx.logout();
             }
         };
         getData();
@@ -27,7 +30,7 @@ const StoreContainer = () => {
     const storesJSX = stores.map((store: Store) => <StoreContainerItem name={store.name} type={store.type} location={store.location} id={store.id} key={store.id}/>);
 
     return <div className="store-container">
-        {storesJSX.length > 0 && storesJSX}
+        {storesJSX.length > 0 && <AvailableStoreList storeNames={stores}/>}
         {storesJSX.length === 0 && <p>No available stores for this location</p>}
     </div>
 };
