30864d89c9e1d515eb733a1ff0a74bdea8a80e1e
Bartoszko <bartosz.bialoglowicz@gmail.com>
2023-07-05 15:19:50 +0200

    feat: Created actions container

---
+++ src/components/Enemies/ActionsContainer.tsx
@@ -0,0 +1,10 @@
+const ActionsContainer = () => {
+    return <div className="actions-container">
+        <div className="action-button"><button></button></div>
+        <div className="action-button"><button></button></div>
+        <div className="action-button"><button></button></div>
+        <div className="action-button"><button></button></div>
+    </div>
+};
+
+export default ActionsContainer;
