c184ffb20504b9fb5bd0ff1c76eda245801f2af4
Bartoszko <bartosz.bialoglowicz@gmail.com>
2023-07-05 15:20:33 +0200

    feat: Created Enemies container

---
+++ src/components/Enemies/EnemiesContainer.tsx
@@ -0,0 +1,42 @@
+import { useContext, useEffect, useState } from "react";
+import { Enemy } from "../../utils/types";
+import { useHttp } from "../../hooks/use-http";
+import { UserContext } from "../../store/user-context";
+import EnemiesList from "./EnemiesList";
+import FightContainer from "./FightContainer";
+
+const EnemiesContainer = () => {
+
+    const [enemies, setEnemies] = useState<Enemy[]>([]);
+    const [isFight, setIsFight] = useState(false);
+    const [pickedEnemy, setPickedEnemy] = useState<Enemy>();
+
+    const userCtx = useContext(UserContext);
+    const sendRequest = useHttp<Enemy[]>('api/enemies', 'GET', undefined, userCtx.user!.authToken);
+
+    useEffect(() => {
+        const getData = async () => {
+            const {data, code} = await sendRequest();
+            if (code === 200) {
+                console.log(data);
+                setEnemies(data);
+            }
+        };
+        getData();
+    },[]);
+
+    const setEnemyHandler = (enemy: Enemy) => {
+        setPickedEnemy(enemy);
+        setIsFight(true);
+    };
+
+    const enemiesJSX = <div className="enemies-container">
+            <EnemiesList enemies={enemies} pickEnemyHandler={setEnemyHandler}/>
+        </div>;
+    
+    const fightJSX = pickedEnemy ?<FightContainer enemy={pickedEnemy}/> : <p>Ooops, something went wrong.</p>
+
+    return isFight ? fightJSX : enemiesJSX;
+};
+
+export default EnemiesContainer;
