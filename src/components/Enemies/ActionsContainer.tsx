826ee631452bfaddcde761bd357f4e2741612e25
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-07-25 21:56:03 +0200

    feat: Added onAttack prop

--- src/components/Enemies/ActionsContainer.tsx
+++ src/components/Enemies/ActionsContainer.tsx
@@ -1,9 +1,9 @@
-const ActionsContainer = () => {
+const ActionsContainer: React.FC<{onAttack: () => void}> = (props) => {
     return <div className="actions-container">
-        <div className="action-button"><button></button></div>
-        <div className="action-button"><button></button></div>
-        <div className="action-button"><button></button></div>
-        <div className="action-button"><button></button></div>
+        <div className="action-button"><button onClick={props.onAttack}>Szybki Atak</button></div>
+        <div className="action-button"><button>Silny Atak</button></div>
+        <div className="action-button"><button>Kula ognia</button></div>
+        <div className="action-button"><button>Ucieczka</button></div>
     </div>
 };
 
