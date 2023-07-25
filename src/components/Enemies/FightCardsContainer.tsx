fc024dcb05a956089c9912ad67c06c9cf44b8d88
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-07-25 21:56:30 +0200

    feat: Added hp bar

--- src/components/Enemies/FightCardsContainer.tsx
+++ src/components/Enemies/FightCardsContainer.tsx
@@ -1,19 +1,29 @@
 import { Enemy } from "../../utils/types";
 
-const FightCardsContainer: React.FC<{enemy: Enemy, enemyCurrentHP: number}> = (props) => {
+import './FightCardsContainer.css';
+
+const FightCardsContainer: React.FC<{enemy: Enemy, enemyCurrentHP: number, myCurrentHP: number, myMaxHP: number}> = (props) => {
+    
+    const myHpBar = props.myCurrentHP > 0 ? `${(props.myCurrentHP / props.myMaxHP) * 100}%` : '0%';
+    const enemyHPBar = props.enemyCurrentHP > 0 ? `${(props.enemyCurrentHP/props.enemy.hp) * 100}%` : '0%';
+
     return <>
         <div className="fight-container-character-card">
             <div className="character-image" >
             </div>
-            <div className="character-hp-bar">
-
+            <div className="character-hp-bar-border">
+                <div className="character-hp-bar" style={{width: myHpBar}}>
+                    {props.myCurrentHP}
+                </div>
             </div>
         </div>
         <div className="fight-container-character-card">
             <div className="character-image" >
             </div>
-            <div className="character-hp-bar">
-                {props.enemyCurrentHP}
+            <div className="character-hp-bar-border">
+                <div className="character-hp-bar" style={{width: enemyHPBar}}>
+                    {props.enemyCurrentHP}
+                </div>
             </div>
         </div>
     </>
