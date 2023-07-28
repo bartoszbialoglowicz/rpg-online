608f3d9efc46398ee7586d70af824fc7167817dc
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-07-28 17:40:38 +0200

    feat: added styles

--- src/components/Enemies/ActionsContainer.tsx
+++ src/components/Enemies/ActionsContainer.tsx
@@ -1,9 +1,11 @@
+import './ActionsContainer.css';
+
 const ActionsContainer: React.FC<{onAttack: () => void}> = (props) => {
     return <div className="actions-container">
-        <div className="action-button"><button onClick={props.onAttack}>Szybki Atak</button></div>
-        <div className="action-button"><button>Silny Atak</button></div>
-        <div className="action-button"><button>Kula ognia</button></div>
-        <div className="action-button"><button>Ucieczka</button></div>
+        <div className="action-button-container"><button onClick={props.onAttack}>Szybki Atak</button></div>
+        <div className="action-button-container"><button>Silny Atak</button></div>
+        <div className="action-button-container"><button>Kula ognia</button></div>
+        <div className="action-button-container"><button>Ucieczka</button></div>
     </div>
 };
 
