9ef50fcd600d67c4f789ede1d15cc68156941005
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-06 19:23:12 +0200

    refactor: new html structure

--- src/components/UI/ItemStats.tsx
+++ src/components/UI/ItemStats.tsx
@@ -2,29 +2,31 @@ import React from "react";
 import { ItemStatsValues } from "../../utils/types";
 
 import './ItemStats.css';
-import { camelCaseToText } from "../../utils/string-modifiers";
 
 const ItemStats:React.FC<ItemStatsValues> = (props) => {
 
-    const getPositiveValue = (value: number) => {
-        // Check if provided valu
-        return value > 0 ? true : false;
-    }
-
-    const setPropsJSX = () => {
-        const propsCopy = props;
-        const jsx = Object.entries(propsCopy).filter((el) => el[0] !== 'hidden').map((el, index) => {
-            if (typeof el[1] === "number") {
-                return  getPositiveValue(el[1]) ? <p key={index}>{`${camelCaseToText(el[0])}: ${el[1]}`}</p> : null;
-            }
-            return null;
-        });
-        return jsx;
-    }
-    const hiddenClass = props.hidden ? 'hidden' : '';
-
-    return <div className={`item-stats ${hiddenClass}`} >
-            {setPropsJSX()}
+    return <div className="item-stats">
+            <div className="item-stats-row">
+                <div className="item-stats-column">
+                    <div className="item-stats-cell">{props.name}</div>
+                </div>
+            </div>
+            <div className="item-stats-row">
+                <div className="item-stats-column">
+                    <div className="item-stats-cell">Damage: {props.damage}</div>
+                    <div className="item-stats-cell">Health: {props.health}</div>
+                </div>
+                <div className="item-stats-column">
+                    <div className="item-stats-cell">Armor: {props.armor}</div>
+                    <div className="item-stats-cell">Magic resist: {props.magicResist}</div>
+                </div>
+            </div>
+            <div className="item-stats-row">
+                <div className="item-stats-column">
+                    <div className="item-stats-cell">Gold value: {props.goldValue}</div>
+                    <div className="item-stats-cell"></div>
+                </div>
+            </div>
         </div>
 };
 
