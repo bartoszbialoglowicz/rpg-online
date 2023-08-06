f0bda6acaf41aad1cd2c23b9e9fbc9406b1ae977
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-06 19:23:31 +0200

    feat: Added stats component

--- src/components/Equipment/Equipment.tsx
+++ src/components/Equipment/Equipment.tsx
@@ -1,8 +1,12 @@
+import CharacterStatsContainer from "../Character/CharacterStatsContainer";
 import EquipmentItemsContainer from "./EquipmentItemsContainer";
 import UserItemContainer from "./UserItemContainer";
 
+import './Equipment.css';
+
 const Equipment = () => {
     return <div className="equipment">
+        <CharacterStatsContainer />
         <EquipmentItemsContainer />
         <UserItemContainer />
     </div>
