8a28d82784c7c91a8624ee89f6418dbe83a2c582
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-25 15:43:40 +0200

    feat: Added utility functions

--- src/components/Equipment/EquipmentItem.tsx
+++ src/components/Equipment/EquipmentItem.tsx
@@ -1,28 +1,57 @@
-import { useState } from "react";
-import { Item } from "../../utils/types";
+import { Item, ItemStatsValues, ItemType } from "../../utils/types";
 
 import './EquipmentItem.css';
 import ItemStats from "../UI/ItemStats";
+import slot from '../../assets/images/empty_slot.jpg.png';
 
-const EquipmentItem: React.FC<Item> = (props) => {
 
-    const [isHidden, setIsHidden] = useState(true);
+type Props = {
+    item?: Item, 
+    removeItemHandler?: (slot: ItemType) => void, 
+    itemType?: string,
+    addItemHandler?: (item: Item) => void,
+    onHoverHandler?: (item: Item) => void,
+    onMouseLeaveHandler?: () => void
+}
 
-    const onMouseHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-        setTimeout(() => setIsHidden((prevState: boolean) => !prevState), 200);
-    }
+const EquipmentItem: React.FC<Props> = (props) => {
+
+    const itemStatsJSX = props.item ? <ItemStats stats={props.item}/> : <ItemStats itemType={props.itemType ? props.itemType : undefined}/>;
     
-    return <div className="equipment-container-item" onMouseEnter={onMouseHandler} onMouseLeave={onMouseHandler}>
-        <div className="equipment-container-item-image">
-            <img src={props.imageUrl} alt={props.name} />
+    const onClickHandler = () => {
+        if (props.removeItemHandler && props.item) {
+            props.removeItemHandler(props.item.itemType);
+        }
+        if (props.addItemHandler && props.item) {
+            props.addItemHandler(props.item);
+        }
+    }
+
+    const onHoverHandler = () => {
+        if (props.onHoverHandler && props.item) {
+            props.onHoverHandler(props.item);
+        }
+    }
+
+    const onMouseLeaveHandler = () => {
+        if (props.onHoverHandler && props.onMouseLeaveHandler) {
+            props.onMouseLeaveHandler();
+        }
+    }
+
+    const backdropJSX = props.item && (props.removeItemHandler || props.addItemHandler) ? <div className="equipment-container-item-stats-backdrop" onMouseOver={onHoverHandler} onMouseLeave={onMouseLeaveHandler}>
+        <button onClick={onClickHandler}>{props.removeItemHandler ? 'ZDEJMIJ' : 'ZAŁÓŻ'}</button>
+        <button>PORÓWNAJ</button>
+    </div> : null;
+
+    return <div className="equipment-container-item">
+            <div className="equipment-container-item-image">
+                <img src={props.item ? props.item.imageUrl : slot} alt={props.item ? props.item.name : 'Empty slot'} />
+            </div>
+            <div className="equipment-container-item-stats">
+                {backdropJSX}
+                {itemStatsJSX}
         </div>
-        <ItemStats
-            name={props.name}
-            armor={props.armor}
-            damage={props.damage}
-            health={props.health}
-            magicResist={props.magicResist}
-            hidden={isHidden}/>
     </div>
 };
 
