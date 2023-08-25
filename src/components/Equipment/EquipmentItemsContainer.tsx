820c38fd420f6c334d30bada75f7687ff707ea44
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-25 15:44:21 +0200

    refactor: Moved eq logic into parent component

--- src/components/Equipment/EquipmentItemsContainer.tsx
+++ src/components/Equipment/EquipmentItemsContainer.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { useHttp } from "../../hooks/use-http";
 import { UserContext } from "../../store/user-context";
 import { Equipment, EquipmentResponseObject, Item, ItemType } from "../../utils/types";
@@ -7,78 +7,47 @@ import EquipmentItem from "./EquipmentItem";
 import './EquipmentItemsContainer.css';
 import CharacterStatsTitle from "../Character/CharacterStatsTitle";
 
-const EquipmentItemsContainer = () => {
-    const userCtx = useContext(UserContext);
-    const sendRequest = useHttp<EquipmentResponseObject[]>('api/equipment', 'GET', undefined, userCtx.user!.authToken);
+const EquipmentItemsContainer: React.FC<{equipment: Equipment, replaceItemHandler: (item: ItemType) => void}> = (props) => {
 
     const [equippedItems, setEquippedItems] = useState<JSX.Element[]>([]);
-    const [equipment, setEquipment] = useState<Equipment>({});
-
-    // Get item value from response object and set it into eq
-    const setItemIntoEq = (eqObj: EquipmentResponseObject) => {
-        const tmpEq = equipment;
-        tmpEq[eqObj.slot] = eqObj.item ? eqObj.item : undefined;
-        setEquipment(tmpEq);
-    }
 
     // Iterate over all slots, then return an JSX Element
     // Return EquipmentItem component with values if slot is not empty
-    // Return <p> with information when slot is empty
+    // Return EquipmentItem component witout props if slot is empty
     // Returned JSX will be used as returned value from this current component
     const setItemsJSX = (items: Item[]) => {
         const tmpJSX: JSX.Element[] = [];
         const types: ItemType[] = ['helmet', 'weapon', 'armor', 'gloves', 'trousers', 'boots'];
         types.forEach((el: string, index: number) => {
-            let tmpJsxEl = <p key={index}>{`${el}: Unequipped`}</p>;
-            items.forEach((item: Item) => {
-                if (item.itemType === el) {
-                    tmpJsxEl = <EquipmentItem 
-                    itemType={item.itemType} 
-                    damage={item.damage}
-                    magicResist={item.magicResist}
-                    health={item.health}
-                    armor={item.armor}
-                    name={item.name}
-                    imageUrl={item.imageUrl}
-                    key={item.name}
-                    />;
+            let tmpJsxEl = <div key={index} className="equipment-container-row">
+                <EquipmentItem itemType={el}/>
+            </div>;
+            const tmpItems = items.map((itemOrBlank: Item | undefined) => {
+                if (itemOrBlank !== undefined) {
+                    return itemOrBlank;
                 }
-                items = items.filter((item: Item) => item.itemType !== el);
+            })
+            tmpItems.forEach((item: Item | undefined) => {
+                if (item !== undefined && item.itemType === el) {
+                    tmpJsxEl = <div key={index} className="equipment-container-row">
+                    <EquipmentItem item={item} removeItemHandler={props.replaceItemHandler}/>
+                    </div>;
+                }
             });
             tmpJSX.push(tmpJsxEl);
-        });
+            });
         return tmpJSX;
     }
 
-    // Get current user's equipment from the API
     useEffect(() => {
-        const getData = async () => {
-            const {data, code} = await sendRequest();
-            if (code === 200) {
-                const tmpItems: Item[] = [];
-                data.forEach((eqObj: EquipmentResponseObject) => {
-                    
-                    if (eqObj.item) {
-                        setItemIntoEq(eqObj);
-                        tmpItems.push(eqObj.item);
-                    }
-                        
-                });
-                setEquippedItems(setItemsJSX(tmpItems));
-            }
-        }
-        getData();
-    }, [])
-
-    const equippedItemsJSX = equippedItems.map((item, key) => {
-        return <div key={key} className="equipment-container-row">
-            {item}
-        </div>
-    })
+        const itemArray = Object.values(props.equipment);
+        setEquippedItems(setItemsJSX(itemArray));
+        console.log("EquipmentItemsCt useEffect");
+    }, [props.equipment]);
 
     return <div className="equipment-container">
         <CharacterStatsTitle title='ekwipunek' />
-        {equippedItemsJSX}
+        {equippedItems}
     </div>
 };
 
