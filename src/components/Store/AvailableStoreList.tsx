d86284cbdd4a30e79c7892804ff9c7af28bfc08e
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-25 15:45:59 +0200

    feat: Added setStore fn prop

--- src/components/Store/AvailableStoreList.tsx
+++ src/components/Store/AvailableStoreList.tsx
@@ -3,12 +3,13 @@ import { Store } from "../../utils/types";
 
 import './AvailableStoreList.css';
 
-const AvailableStoreList: React.FC<{storeNames: Store[]}> = (props) => {
-    const storesListJSX = props.storeNames.map((store: Store) => {
-        return <div className="store-container-store-list-option" key={store.id}>
+const AvailableStoreList: React.FC<{stores: Store[], setStore: (store: Store) => void}> = (props) => {
+    
+    const storesListJSX = props.stores.length > 0 ? props.stores.map((store: Store) => {
+        return <div className="store-container-store-list-option" key={store.id} onClick={() => props.setStore(store)}>
             <p>{`${store.name} (${store.type})`}</p>
         </div>
-    })
+    }) : <p>Brak sklepów w obecnej lokalizacji</p>
 
     return <div className="store-container-store-list">
         {storesListJSX}
