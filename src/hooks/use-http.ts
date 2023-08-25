324e941463798d4a7207ba9e200627003e021af2
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-25 15:42:34 +0200

    feat: Added optional args

--- src/hooks/use-http.ts
+++ src/hooks/use-http.ts
@@ -2,16 +2,18 @@ import { AppSettings } from "../utils/settings";
 import { HttpMethod, responseObject } from "../utils/types";
 
 export const useHttp = <objType>(path: string, method: HttpMethod, body?: object, token?: string) => {
-    const sendRequest = async () => {
-        const response = await fetch(`http://${AppSettings.SERVER_IP}/${path}`, {
-            method: method,
+    const sendRequest = async (path_2?: string, method_2?: HttpMethod, body_2?: object) => {
+        const url = path_2 ? path_2 : path;
+        const response = await fetch(`http://${AppSettings.SERVER_IP}/${url}`, {
+            method: method_2 ? method_2 : method,
             headers: {
                 'Content-Type': 'application/json',
                 'Accept': 'application/json',
                 'Authorization': token ? `Token ${token}` : ''
             },
-            body: body ? JSON.stringify(body) : null
+            body: body_2 ? JSON.stringify(body_2) : body ? JSON.stringify(body) : null,
         });
+        
         const data: objType = await response.json();
         const code = response.status;
         const responseData: responseObject<objType> = {
