aac6272fb1d6c008a5ae7c578b65c35249dc269d
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-25 15:44:35 +0200

    refactor: Removed trash code

--- src/components/Equipment/EquipmentPotion.tsx
+++ src/components/Equipment/EquipmentPotion.tsx
@@ -1,24 +1,15 @@
-import { useState } from "react";
 import { Potion } from "../../utils/types";
 
 import './EquipmentItem.css';
-import ItemStats from "../UI/ItemStats";
 
 const EquipmentPotion: React.FC<Potion> = (props) => {
 
-    const [isHidden, setIsHidden] = useState(true);
 
-    const onMouseHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-        setTimeout(() => setIsHidden((prevState: boolean) => !prevState), 200);
-    }
-
-    return <div className="equipment-container-item" onMouseEnter={onMouseHandler} onMouseLeave={onMouseHandler}>
+    return <div className="equipment-container-item" >
         <p>{props.name}</p>
         <p>Potion</p>
-        <ItemStats
-            hpValue={props.hpValue}
-            goldValue={props.goldValue}
-            hidden={isHidden}/>
+        <p>{`Leczy ${props.hpValue} HP`}</p>
+        <p>{`Wartość: ${props.goldValue}`}</p>
     </div>
 };
 
