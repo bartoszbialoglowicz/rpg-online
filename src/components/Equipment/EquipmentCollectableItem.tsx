df160b317ab31fa028302c3173b5cd3abb537292
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-08-25 15:40:31 +0200

    refactor: Removed trash code

--- src/components/Equipment/EquipmentCollectableItem.tsx
+++ src/components/Equipment/EquipmentCollectableItem.tsx
@@ -1,20 +1,10 @@
-import { useState } from "react";
 import { CollectableItem } from "../../utils/types";
-import ItemStats from "../UI/ItemStats";
 
 const EquipmentCollectableItem: React.FC<CollectableItem> = (props) => {
-    const [isHidden, setIsHidden] = useState(true);
-
-    const onMouseHandler = (event: React.MouseEvent<HTMLDivElement>) => {
-        setTimeout(() => setIsHidden((prevState: boolean) => !prevState), 200);
-    }
-    
-    return <div className="equipment-container-item" onMouseEnter={onMouseHandler} onMouseLeave={onMouseHandler}>
+    return <div className="equipment-container-item">
         <p>{props.name}</p>
         <p>Item</p>
-        <ItemStats
-            goldValue={props.goldValue}
-            hidden={isHidden}/>
+        <p>{`Wartość: ${props.goldValue}`}</p>
     </div>
 };
 
