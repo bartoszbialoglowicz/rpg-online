801c76606ace99f399475431047b6cb701f87c2c
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-10-08 18:01:47 +0200

    feat: Moved API request onto parent component

--- src/components/Equipment/UserItemContainer.tsx
+++ src/components/Equipment/UserItemContainer.tsx
@@ -1,7 +1,4 @@
-import { useContext, useEffect, useState } from "react";
-import { useHttp } from "../../hooks/use-http";
-import { UserContext } from "../../store/user-context";
-import { InventoryResponseObject, Item, ItemStatsValues, Potion, UserItemsResponseObject } from "../../utils/types";
+import { ButtonController, Item, Potion } from "../../utils/types";
 import EquipmentItem from "./EquipmentItem";
 import { CollectableItem } from "../../utils/types";
 import EquipmentCollectableItem from "./EquipmentCollectableItem";
@@ -10,46 +7,25 @@ import EquipmentPotion from "./EquipmentPotion";
 import './UserItemsContainer.css';
 import CharacterStatsTitle from "../Character/CharacterStatsTitle";
 
-const UserItemContainer: React.FC<{addItemHandler: (item:Item) => void, onHoverHandler: (item: Item) => void, onMouseLeaveHandler?: () => void}> = (props) => {
+const UserItemContainer: React.FC<{
+    items: Item[],
+    potions: Potion[],
+    collectableItems: CollectableItem[],
+    buttons?: ButtonController[],
+    onHoverHandler?: (item: Item) => void, 
+    onMouseLeaveHandler?: () => void
+    }> = (props) => {
 
-    const userCtx = useContext(UserContext);
-    const sendRequest = useHttp<InventoryResponseObject>('api/inventory/', "GET", undefined, userCtx.user?.authToken);
-    const [items, setItems] = useState<Item[]>([]);
-    const [potions, setPotions] = useState<Potion[]>([]);
-    const [collectableItems, setCollectableItems] = useState<CollectableItem[]>([]);
-
-    const setEqItemHandler = (item: Item) => {
-        props.addItemHandler(item);
-    };
-
-    useEffect(() => {
-        const getItems = async () => {
-            const {data, code} = await sendRequest();
-            if (code === 200) {
-                const responseItems = data.items.map((item) => item.item);
-                const responseCollectables = data.collectableItems.map(el => el.collectableItem)
-                const responsePotions = data.potions.map(el => el.potion);
-                setCollectableItems(responseCollectables);
-                setPotions(responsePotions);
-                setItems(responseItems);
-            }
-            else {
-                console.log(data);
-            }
-        };
-
-        getItems();
-    }, []);
-
-    const itemsJsx = items.map((item: Item, index: number) => {
-        return <EquipmentItem item={item} key={index} addItemHandler={setEqItemHandler} onHoverHandler={props.onHoverHandler} onMouseLeaveHandler={props.onMouseLeaveHandler}/>
+    const itemsJsx = props.items.map((item: Item, index: number) => {
+        const buttons = props.buttons ? props.buttons : [];
+        return <EquipmentItem item={item} key={index} buttons={buttons} onHoverHandler={props.onHoverHandler} onMouseLeaveHandler={props.onMouseLeaveHandler} />
     });
-    const collectableJsx = collectableItems.map((item: CollectableItem) => {
+    const collectableJsx = props.collectableItems.map((item: CollectableItem) => {
         return <EquipmentCollectableItem name={item.name} goldValue={item.goldValue} id={item.id} key={item.id}/>
-    })
-    const potionJsx = potions.map((item: Potion) => {
+    });
+    const potionJsx = props.potions.map((item: Potion) => {
         return <EquipmentPotion name={item.name} hpValue={item.hpValue} goldValue={item.goldValue} id={item.id} key={item.id}/>
-    })
+    });
 
     return <div className="user-items-container">
         <CharacterStatsTitle title='Twoje przedmioty' />
