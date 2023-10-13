a11bae823f2217fde43de062a01755a80f0a1b24
Bartoszko <bartosz.bialoglowicz@gmail.com>
2023-10-13 13:40:32 +0200

    feat: Updated server address

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static readonly SERVER_IP = 'https://bbialoglowicz.pythonanywhere.com/';
+    public static readonly SERVER_IP = 'bbialoglowicz.pythonanywhere.com/';
     public static readonly PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
     public static readonly EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     public static readonly NICKNAME_REGEX = /^(?!.*\d)[\s\S]{6,16}$/;
