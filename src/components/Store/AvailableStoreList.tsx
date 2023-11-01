786942720f7ee420fa3256225409547ba832ac3f
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-11-01 15:06:20 +0100

    feat: Added NPC image list

--- src/components/Store/AvailableStoreList.tsx
+++ src/components/Store/AvailableStoreList.tsx
@@ -8,6 +8,15 @@ const AvailableStoreList: React.FC<{stores: Store[], setStore: (store: Store) =>
     const storesListJSX = props.stores.length > 0 ? props.stores.map((store: Store) => {
         return <div className="store-container-store-list-option" key={store.id} onClick={() => props.setStore(store)}>
             <p>{`${store.name} (${store.type})`}</p>
+            <div className="store-container-shopkeeper">
+                <div className="store-container-shopkeeper-image">
+                    <img src={store.npc.imageUrl} alt={store.npc.name} />
+                </div>
+                <div className="store-container-shopkeeper-name">
+                    <h2>{store.npc.name}</h2>
+                    <h3>{store.type}</h3>
+                </div>
+            </div>
         </div>
     }) : <p>Brak sklepów w obecnej lokalizacji</p>
 
