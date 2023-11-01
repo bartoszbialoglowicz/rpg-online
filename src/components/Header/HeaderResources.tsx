8eb7ae6266455159ec17340eabeb66afe383bf46
bartoszbialoglowicz <bartosz.bialoglowicz@gmail.com>
2023-11-01 15:05:23 +0100

    feat: Added logout

--- src/components/Header/HeaderResources.tsx
+++ src/components/Header/HeaderResources.tsx
@@ -4,11 +4,14 @@ import HeaderLvlBar from "./HeaderLvlBar";
 
 import './HeaderResources.css';
 import coins from '../../assets/images/coins.png';
+import { UserContext } from "../../store/user-context";
 
 const HeaderResources = () => {
     
     const gameCtx = useContext(GameContext);
     
+    const userCtx = useContext(UserContext);
+
     const [resources, setResources] = useState(gameCtx.resources);
 
     useEffect(() => {
@@ -17,6 +20,7 @@ const HeaderResources = () => {
     return <div className="header-resources">
         <div className="header-resources-gold">{resources.gold} <img src={coins} alt="coins"></img></div>
         <HeaderLvlBar currentEXP={resources.lvl.currentExp} lvlExpLimit={resources.lvl.expPointsGap} lvl={resources.lvl.lvl}/>
+        <div className="header-logout" onClick={userCtx.logout}>LOGOUT</div>
     </div>
 };
 
