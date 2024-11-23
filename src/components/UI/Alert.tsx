7c81a2d8d67dcec17b9789ad8b5464aeed9ece7f
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-11-23 22:07:46 +0100

    feat: Replaced button with UI component

--- src/components/UI/Alert.tsx
+++ src/components/UI/Alert.tsx
@@ -1,12 +1,12 @@
+import Button from "./Button";
 import Modal from "./Modal";
-import SubmitInput from "./SubmitInput";
 
 const Alert: React.FC<{title: string, description: string, buttonText: string, onButtonClick: () => void, onOutOfBoxClickHandler: () => void}> = (props) => {
     return <Modal onClickHandler={props.onOutOfBoxClickHandler}>
         <div className="alert">
         <h2>{props.title}</h2>
         <p>{props.description}</p>
-        <button onClick={props.onButtonClick}>{props.buttonText}</button>
+        <Button text={props.buttonText} onClickHandler={props.onButtonClick}/>
         </div>
     </Modal>
     
