7848348da59bdf0b02c099811de7338efa40b667
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-11-23 22:08:11 +0100

    feat: Added children prop

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,6 +1,12 @@
-const Card = () => {
+import './Card.css';
+
+type Props = {
+    children: JSX.Element
+}
+
+const Card: React.FC<Props> = (props) => {
     return <div className="card">
-        
+        {props.children}
     </div>
 };
 
