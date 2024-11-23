707ceca5070d602c63675d9b966504a32e122765
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-11-23 21:43:19 +0100

    feat: Added enemy Item component

---
+++ src/components/Enemies/EnemiesListItem.tsx
@@ -0,0 +1,31 @@
+import { Enemy } from "../../utils/types";
+import Button from "../UI/Button";
+
+import './EnemiesListItem.css';
+
+type Props = {
+    enemy: Enemy,
+    onClickHandler: (enemy: Enemy) => void;
+}
+
+const EnemiesListItem: React.FC<Props> = (props) => {
+
+    const onClickHandler = () => {
+        props.onClickHandler(props.enemy);
+    }
+
+    return <div className="enemies-list-item">
+        <div className="enemies-list-item-image">
+            <img src={props.enemy.imgSrc} alt={props.enemy.name}/>
+        </div>
+        <div className="enemies-list-item-info">
+            <h3>{props.enemy.name}</h3>
+            <i>{`Poziom: ${props.enemy.lvl}`}</i>
+        </div>
+        <div className="enemies-list-item-action">
+            <Button text="ATAKUJ" onClickHandler={onClickHandler}/>
+        </div>
+    </div>
+};
+
+export default EnemiesListItem;
