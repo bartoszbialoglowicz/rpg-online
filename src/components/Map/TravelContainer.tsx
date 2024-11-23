e1175c29a5aa1254f818d4f611efc41dd324e1e6
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-11-23 22:07:13 +0100

    feat: Created travel component

---
+++ src/components/Map/TravelContainer.tsx
@@ -0,0 +1,22 @@
+import { Location } from "../../utils/types";
+import LoadingBar from "../UI/LoadingBar";
+
+import './TravelContainer.css';
+
+type Props = {
+    startTravelTime: Date,
+    travelTime: Date,
+    remainingTime: string
+}
+
+const TravelContainer: React.FC<Props> = (props) => {
+
+
+    return <div className="travel-container">
+        <h2>Obecnie podróżujesz...</h2>
+        <LoadingBar minValue={props.startTravelTime.getTime()} maxValue={props.travelTime.getTime()} currentValue={new Date().getTime()} barText={props.remainingTime}/>
+    </div>
+};
+
+export default TravelContainer;
+
