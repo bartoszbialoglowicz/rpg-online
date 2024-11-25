8a548df133ee141ac608ab4618d24f34731a0d24
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-11-25 23:02:06 +0100

    test: Testing changes

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static readonly SERVER_IP = '127.0.0.1:8000';
+    public static readonly SERVER_IP = 'frog02.mikr.us:20928';
     public static readonly PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
     public static readonly EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     public static readonly NICKNAME_REGEX = /^(?!.*\d)[\s\S]{6,16}$/;
