952b6ac59d1badd24be1dfe6b454a42b397c0497
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-12-06 17:48:29 +0100

    feat: Added card animations

--- src/components/Enemies/FightCardsContainer.tsx
+++ src/components/Enemies/FightCardsContainer.tsx
@@ -2,14 +2,33 @@ import { Enemy } from "../../utils/types";
 
 import './FightCardsContainer.css';
 import avatar1 from '../../assets/images/tmp_avatar.png';
+import { useEffect, useState } from "react";
 
 const FightCardsContainer: React.FC<{enemy: Enemy, enemyCurrentHP: number, myCurrentHP: number, myMaxHP: number}> = (props) => {
     
     const myHpBar = props.myCurrentHP > 0 ? `${Math.floor((props.myCurrentHP / props.myMaxHP) * 100)}%` : '0%';
     const enemyHPBar = props.enemyCurrentHP > 0 ? `${Math.floor((props.enemyCurrentHP/props.enemy.health) * 100)}%` : '0%';
 
+    const [userLastHP, setUserLastHP] = useState(props.myCurrentHP);
+    const [enemyLastHP, setEnemyLastHP] = useState(props.enemy.health);
+    const [userCardAnimations, setUserCardAnimations] = useState("");
+    const [enemyCardAnimations, setEnemyCardAnimations] = useState("");
+
+    useEffect(() => {
+        setEnemyCardAnimations("");
+        setUserCardAnimations("");
+        if (props.enemyCurrentHP !== enemyLastHP) {
+            setEnemyLastHP(props.enemyCurrentHP);
+            setEnemyCardAnimations("test");
+        }
+        if (userLastHP !== props.myCurrentHP) {
+            setUserLastHP(props.myCurrentHP);
+            setUserCardAnimations("test");
+        }
+    }, [props.enemyCurrentHP, props.myCurrentHP]);
+
     return <div className="fight-cards-container">
-        <div className="fight-container-character-card">
+        <div className={`fight-container-character-card ${userCardAnimations}`}>
             <div className="character-image" >
                 <img src={avatar1} alt="User avatar">
                 </img>
@@ -21,7 +40,7 @@ const FightCardsContainer: React.FC<{enemy: Enemy, enemyCurrentHP: number, myCur
                 <div className="character-hp-bar-text">{myHpBar}</div>
             </div>
         </div>
-        <div className="fight-container-character-card">
+        <div className={`fight-container-character-card ${enemyCardAnimations}`}>
             <div className="character-image" >
                 <img src={props.enemy.imgSrc} alt="Enemy avatar" />
             </div>
