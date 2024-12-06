6f6556566c426ce3dcf1fe88c1a7dbd00ec260d4
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-12-06 17:51:48 +0100

    feat: Added child prop

--- src/components/UI/Alert.tsx
+++ src/components/UI/Alert.tsx
@@ -1,11 +1,21 @@
 import Button from "./Button";
 import Modal from "./Modal";
 
-const Alert: React.FC<{title: string, description: string, buttonText: string, onButtonClick: () => void, onOutOfBoxClickHandler: () => void}> = (props) => {
+type Props = {
+    title: string, 
+    description: string, 
+    buttonText: string, 
+    onButtonClick: () => void, 
+    onOutOfBoxClickHandler: () => void,
+    children?: JSX.Element
+};
+
+const Alert: React.FC<Props> = (props) => {
     return <Modal onClickHandler={props.onOutOfBoxClickHandler}>
         <div className="alert">
         <h2>{props.title}</h2>
         <p>{props.description}</p>
+        {props.children && <div>{props.children}</div>}
         <Button text={props.buttonText} onClickHandler={props.onButtonClick}/>
         </div>
     </Modal>
