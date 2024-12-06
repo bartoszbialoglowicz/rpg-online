f47a2b9ae1562dd86ae1cc82a098104266f88536
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-12-06 17:49:54 +0100

    feat: Added content render

--- src/components/Inventory/ItemsContainer.tsx
+++ src/components/Inventory/ItemsContainer.tsx
@@ -1,7 +1,23 @@
+import { useContext } from "react";
+import { InventoryContext } from "../../store/inventory-context";
+import ItemContainer from "./ItemContainer";
+
+import './ItemsContainer.css';
 const ItemsContainer = () => {
 
+    const inventoryCtx = useContext(InventoryContext);
+
+    const items = inventoryCtx.inventory.items.map(item =>
+        <ItemContainer item={item.item} key={item.id} onClick={inventoryCtx.replaceEquipmentItem}/>
+    );
+
+    const nums: number[] = [...Array(40-inventoryCtx.inventory.items.length)];
+
+    const it = nums.map(el => <div className="item-container empty" />)
+
     return <div className="items-container">
-        
+        {items}
+        {it}
     </div>
 };
 
