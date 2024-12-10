1cd0f822de8c0248291591ed611d0f82fd2d4247
Bartoszko <bartosz.bialoglowicz@gmail.com>
2024-12-10 23:51:05 +0100

    refactor: Changed text language

--- src/components/Auth/LoginFormContainer.tsx
+++ src/components/Auth/LoginFormContainer.tsx
@@ -9,10 +9,10 @@ import './LoginFormContainer.css';
 const LoginFormContainer: React.FC<{changeFormHandler: (type: boolean) => void, setFeedbackHandler: (text: string, type: feedbackResult) => void, feedbackResult?: feedbackResult, feedbackText?: string}> = (props) => {
 
     return <div className="form-container">
-        <FormTitle formTitle="SIGN IN"/> 
+        <FormTitle formTitle="LOGOWANIE"/> 
         <LoginForm  setfeedbackHandler={props.setFeedbackHandler}/>
         {props.feedbackResult && props.feedbackText && <FormFeedback result={props.feedbackResult} text={props.feedbackText}/>}
-        <FormChange text="Do not have an account? Register here!" setFormTypeHandler={props.changeFormHandler} formType={false}/>
+        <FormChange text="Jeżeli nie masz konta zarejestruj się tutaj" setFormTypeHandler={props.changeFormHandler} formType={false}/>
     </div>
 };
 
