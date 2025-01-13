524e495a944cee1346d77dd4bb37a196508dbc9f
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-01-13 22:28:00 +0100

    refactor: Removed dead code

--- src/components/Store/StoreItemsContainer.tsx
+++ src/components/Store/StoreItemsContainer.tsx
@@ -1,6 +1,5 @@
 import { Item } from '../../types/ItemTypes';
 import { StoreItem } from '../../types/StoreTypes';
-import { ButtonController } from '../../utils/types';
 import ItemContainer from '../Inventory/ItemContainer';
 
 import './StoreItemsContainer.css';
@@ -11,14 +10,7 @@ const StoreItemsContainer: React.FC<{storeItems: StoreItem[], addTransactionItem
         return {...el.item}
     });
 
-    const getItemHandler = (item: Item) => {
-        const tmpItem = props.storeItems.find(el => el.item.id === item.id);
-        if (tmpItem)
-            props.addTransactionItemHandler(tmpItem);
-    }
-
     const itemsJSX = itemsList.map(el => {
-        const button: ButtonController = {onClick: () => {getItemHandler(el)}, text:"KUP"}
         return <ItemContainer item={el}/>
     })
 
