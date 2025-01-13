7cf9394b25d116f45bfc4509c04341a13f8aa356
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-01-13 22:26:33 +0100

    feat: Added npc image

--- src/components/Store/AvailableStoreList.tsx
+++ src/components/Store/AvailableStoreList.tsx
@@ -2,13 +2,16 @@ import React from "react";
 import { Store } from "../../types/StoreTypes";
 
 import './AvailableStoreList.css';
+import Card from "../UI/Card";
 
 const AvailableStoreList: React.FC<{stores: Store[], setStore: (store: Store) => void}> = (props) => {
     
     const storesListJSX = props.stores.length > 0 ? props.stores.map((store: Store) => {
-        return <div className="store-container-store-list-option" key={store.id} onClick={() => props.setStore(store)}>
-            <p>{`${store.name} (${store.type})`}</p>
-        </div>
+        return <Card>
+            <div className="store-container-store-list-option" key={store.id} onClick={() => props.setStore(store)}>
+                <img src={store.npc.imageUrl} alt={store.npc.name} />
+            </div>
+            </Card>
     }) : <p>Brak sklepów w obecnej lokalizacji</p>
 
     return <div className="store-container-store-list">
