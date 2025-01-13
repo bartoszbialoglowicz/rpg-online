9e136b06a75dc9b730c3149a0ef9dcb5115daff0
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-01-13 22:23:47 +0100

    feat: Mounted item comparator component

--- src/components/Inventory/InventoryContainer.tsx
+++ src/components/Inventory/InventoryContainer.tsx
@@ -1,4 +1,5 @@
 import StatsContainer from "../Stats/StatsContainer";
+import ComparatorContainer from "./ComparatorContainer";
 import EquipmentContainer from "./EquipmentContainer";
 
 import './InventoryContainer.css';
@@ -7,6 +8,7 @@ import ItemsContainer from "./ItemsContainer";
 const InventoryContainer = () => {
 
     return <div className="inventory-container">
+        <ComparatorContainer />
         <StatsContainer />
         <EquipmentContainer />
         <ItemsContainer />
