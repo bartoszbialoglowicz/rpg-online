3de8ca42a61acbc781950adc907d1bd9364a941f
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-01-13 22:22:44 +0100

    feat: Added message types handlers

--- src/components/Enemies/FightContainer.tsx
+++ src/components/Enemies/FightContainer.tsx
@@ -1,18 +1,20 @@
 import { useContext, useEffect, useState } from "react";
-import { Enemy, WebSocketMessage } from "../../utils/types";
 import ActionsContainer from "./ActionsContainer";
 import FightCardsContainer from "./FightCardsContainer";
 import WebsocketService from "../../services/websocketService";
 import { UserContext } from "../../store/user-context";
 import { GameContext } from "../../store/game-context";
 import Alert from "../UI/Alert";
-import { Stats } from "../../types/GameTypes";
+import { Enemy, Stats, WebSocketMessage } from "../../types/GameTypes";
 import { StatsContext } from "../../store/stats-context";
 import { InventoryContext } from "../../store/inventory-context";
 import { Item } from "../../types/ItemTypes";
 import { AppSettings } from "../../utils/settings";
 import ItemContainer from "../Inventory/ItemContainer";
 import useAudio from "../../hooks/use-audio";
+
+import './FightContainer.css';
+import FightLogsContainer from "./FightLogsContainer";
 
 type Props = {
     enemy: Enemy,
@@ -31,8 +33,13 @@ const FightContainer: React.FC<Props> = (props) => {
     const [alertIsVisible, setAlertIsVisible] = useState(false);
     const [fightIsOver, setFightIsOver] = useState(false);
     const [serverMessage, setServerMessage] = useState('');
+    const [endFightMessage, setEndFightMessage] = useState('');
     const [loot, setLoot] = useState<Item>();
     const [lootJSX, setLootJSX] = useState<JSX.Element>();
+    const [turnLocked, setTurnLocked] = useState(false);
+    const [logs, setLogs] = useState<string[]>([]);
+
+    const playerMaxHP = statsContext.getAllStats().health;
 
     const normalHitSound = require('../../assets/audio/normal_hit.wav');
     const criticallHitSound = require('../../assets/audio/critical_hit.wav');
@@ -47,6 +54,24 @@ const FightContainer: React.FC<Props> = (props) => {
         if (isPlaying){
             toggle();
         }
+    }
+
+    const addLogHandler = (wsMessage: WebSocketMessage) => {
+
+        const updateState = (msg: string) => { setLogs(prevState => [...prevState, msg])}
+        const damageDealtMsg = (name: string, val: number, isCrit?: boolean) => isCrit ? `${name} uderzył krytycznie zadając ${val} obrażeń!` : `${name} zadał ${val} obrażeń.`;
+        const noDamageDealt = (name: string) => `${name} swoim ciosem nie zdołał zadać żadnych obrażeń.`;
+
+        if (wsMessage.userDamageDealt)
+            updateState(damageDealtMsg(authCtx.user!.nickname, wsMessage.userDamageDealt, wsMessage.criticalHit));
+        else if (wsMessage.enemyDamageDealt)
+            updateState(damageDealtMsg(props.enemy.name, wsMessage.enemyDamageDealt, wsMessage.criticalHit));
+        else if (wsMessage.enemyDamageDealt === 0)
+            updateState(noDamageDealt(props.enemy.name));
+        else if (wsMessage.userDamageDealt === 0)
+            updateState(noDamageDealt(authCtx.user!.nickname));
+        else if (wsMessage.message)
+            updateState(wsMessage.message)
     }
 
     const playAttackSound = (critical: boolean) => {
@@ -64,6 +89,9 @@ const FightContainer: React.FC<Props> = (props) => {
     }
 
     const handleWebSocketMessage = (data: WebSocketMessage) => {
+        // Add fight logs
+        addLogHandler(data);
+
         if (data.character) {
             playAttackSound(data.criticalHit!);
             setCharacterStats(data.character);
@@ -75,6 +103,8 @@ const FightContainer: React.FC<Props> = (props) => {
         if (data.message) {
             setServerMessage(data.message);
         }
+        if (data.messageShort)
+            setEndFightMessage(data.messageShort);
         if (data.fightIsOver) {
             setFightIsOver(data.fightIsOver);
             if (data.exp && data.expPoints && data.lvl) {
@@ -87,15 +117,20 @@ const FightContainer: React.FC<Props> = (props) => {
             loot.imageUrl = getAbsoluteUrl(loot.imageUrl!);
             setLoot(data.loot);
             setLootJSX(<ItemContainer item={loot}/>);
+            inventoryCtx.addInventoryItem(data.loot);
         }
+
+        
     }
 
     const handleAttack = () => {
-        if (!fightIsOver) {
+        if (!fightIsOver && !turnLocked) {
+            setTurnLocked(true);
             WebsocketService.sendMessage({action: 'user_attack'})
             setTimeout(() => {
                 if (!fightIsOver)
-                    WebsocketService.sendMessage({action: 'enemy_attack'})
+                    WebsocketService.sendMessage({action: 'enemy_attack'});
+                setTurnLocked(false);
             }, 1000)
         }
         
@@ -123,15 +158,20 @@ const FightContainer: React.FC<Props> = (props) => {
 
     return <div className="fight-container">
         {alertIsVisible && <Alert 
-            title={serverMessage} 
-            description={!loot ? "Nie zdobyto łupów." : `Zdobyto ${loot.name}`}
+            title={endFightMessage} 
+            description={!loot ? "Nie zdobyto łupów." : `Zdobyto łupy:`}
             buttonText={!loot  ? "OPUŚĆ WALKĘ" : "ZABIERAM"}
             onButtonClick={hideAlertHandler} 
             onOutOfBoxClickHandler={hideAlertHandler}
             children={lootJSX}
             />}
-        <FightCardsContainer enemy={props.enemy} enemyCurrentHP={enemyStats.health} myCurrentHP={characterStats.health} myMaxHP={characterStats.health}/>
-        <ActionsContainer onAttack={handleAttack}/> 
+            <div className="fight-top-container">
+                <FightCardsContainer enemy={props.enemy} enemyCurrentHP={enemyStats.health} myCurrentHP={characterStats.health} myMaxHP={playerMaxHP}/>
+                <FightLogsContainer logs={logs} />
+            </div>
+            <div className="fight-bottom-container">
+                <ActionsContainer onAttack={handleAttack}/>
+            </div>
     </div>
 };
 
