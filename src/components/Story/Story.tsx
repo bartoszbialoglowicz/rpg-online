ef7a170e5004a96696693f653867208ec6a516f3
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-03-16 21:20:12 +0100

    feat: Initialized story component

---
+++ src/components/Story/Story.tsx
@@ -0,0 +1,14 @@
+import { useEffect, useState } from "react";
+import DialogContainer from "./DialogContainer";
+
+type Props = {
+    initialDialogId: number
+}
+
+const Story: React.FC<Props> = (props) => {
+    
+
+    return <div className="story"></div>
+};
+
+export default Story;
