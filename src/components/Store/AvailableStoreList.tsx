3695a2db8099dd05af71e2d12fabc77cc607e42f
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-03-16 21:19:46 +0100

    fix: Store does not appear while travelling

--- src/components/Store/AvailableStoreList.tsx
+++ src/components/Store/AvailableStoreList.tsx
@@ -1,18 +1,23 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Store } from "../../types/StoreTypes";
 
 import './AvailableStoreList.css';
 import Card from "../UI/Card";
+import { GameContext } from "../../store/game-context";
 
 const AvailableStoreList: React.FC<{stores: Store[], setStore: (store: Store) => void}> = (props) => {
     
+    const gameCtx = useContext(GameContext);
+
     const storesListJSX = props.stores.length > 0 ? props.stores.map((store: Store) => {
         return <Card>
             <div className="store-container-store-list-option" key={store.id} onClick={() => props.setStore(store)}>
                 <img src={store.npc.imageUrl} alt={store.npc.name} />
             </div>
             </Card>
-    }) : <p>Brak sklepów w obecnej lokalizacji</p>
+    }) : <div className="store-container-store-list-empty">Brak sklepów w pobliżu
+        <img src={gameCtx.userLocation.location.imageUrl} />
+    </div>;
 
     return <div className="store-container-store-list">
         {storesListJSX}
