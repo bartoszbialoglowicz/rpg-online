f4c8101ccb072396c1704eba8c46273832cafad8
Bartoszko <bartosz.bialoglowicz@gmail.com>
2025-04-28 20:25:48 +0200

    feat: Added new element for content type

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -1,5 +1,5 @@
 export class AppSettings {
-    public static readonly SERVER_IP = '127.0.0.1:8000';
+    public static readonly SERVER_IP = 'frog02-20928.wykr.es';
     public static readonly PASSWORD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d@$!%*?&]{8,}$/;
     public static readonly EMAIL_REGEX = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     public static readonly NICKNAME_REGEX = /^[\s\S]{6,64}$/;
@@ -11,4 +11,4 @@ export class InterfaceSettings {
     public static readonly STORE_GRID_COLUMNS = 10;
 }
 
-export type mainContentName = 'CHARACTER' | 'NEWS' | 'EQUIPMENT' | 'MAP' | 'STORE' | 'ENEMIES' | 'QUESTS';
+export type mainContentName = 'CHARACTER' | 'STAGE' | 'EQUIPMENT' | 'MAP' | 'STORE' | 'ENEMIES' | 'QUESTS';
